a6fc2ae01958322a49ca29b9bd51d0140e1e3c37
MariamWand <mariamwandaogo7@gmail.com>
2023-06-16 09:09:33 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+
+ return(
+  
+  <React.Fragment>
+    <Navbar/>
+  </React.Fragment>
+
+ )
+
+}
+
+export default App;
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+ import {FaBars, FaTimes} from "react-icons/fa"
+
+function Navbar() {
+
+   
+    return (
+            <header>
+                <h3>Logo</h3>
+                <nav>
+                    <a href="/#">home</a>
+                    <a href="/#">home</a>
+                    <a href="/#">home</a>
+                    <a href="/#">home</a>
+
+                    <button>
+                        sa
+                    </button>
+                </nav>
+            </header>
+    );
+ 
+}
+
+export default Navbar;
